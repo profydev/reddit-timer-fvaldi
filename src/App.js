cddc3e303f7ba8ee875c2e3c3974bf08e95c1f43
Valdi <fvaldi@gmail.com>
2022-05-07 23:22:25 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Search from './pages/search';
 import 'normalize.css';
 import './styles/global.css';
 
 function App() {
-  return <div>App Placeholder</div>;
+  return (
+    <BrowserRouter>
+      <div>App Placeholder</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
